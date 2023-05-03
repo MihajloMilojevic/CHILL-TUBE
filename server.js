a6869ae9aa04957fd01b994c7f3c9a9469a40ba6
Mihajlo Milojevic <milojevicm374@gmail.com>
2023-05-04 00:15:03 +0200

    custom server upgrade - serve static files in production

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
-const express = require('express');
-const next = require('next');
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+const path = require("path");
+const express = require("express");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = process.env.PORT || 3000
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = express();
+try {
+  console.log("BEFORE prepare")
+  app.prepare().then(() => {
+    const server = express();
   
-  server.all('*/*', (req, res) => {
-    return handle(req, res);
-  });
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+    console.log("BEFORE use")
+    // Serve static files from the uploads folder
+    server.use(express.static('public'));
+  
+    console.log("BEFORE handle")
+    // Handle all other requests with Next.js
+    server.all('*', (req, res) => {
+      return handle(req, res);
+    });
+  
+    const httpServer = createServer(server);
+  
+    console.log("BEFORE LISTEN")
+    httpServer.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  }).catch(e => {
+    console.log("PREPARE CATCH");
+    console.error(e);
+  })
+} catch (error) {
+  console.log("error")
+  console.error(error);
+}
+
+console.log("end")
